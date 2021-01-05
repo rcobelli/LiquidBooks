35b1304b1c7a373f2c07d8717a4e8765c3ff6b0e
Ryan Cobelli <ryan.cobelli@rybel-llc.com>
2021-01-05 16:05:10 -0500

    Use new debug cookie

--- init.php
+++ init.php
@@ -1,9 +1,12 @@
 <?php
 
-error_reporting(0);
-//ini_set('display_errors',1);
-//ini_set('display_startup_errors',1);
-//error_reporting(-1);
+if ($_COOKIE['debug'] == 'true') {
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(-1);
+} else {
+    error_reporting(0);
+}
 
 include_once("stdlib.php");
 

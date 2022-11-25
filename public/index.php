62466011ff55e01d673e4ec066b02f30432b8361
Ryan Cobelli <ryan.cobelli@rybel-llc.com>
2022-11-25 18:57:19 -0500

    Fix link on dashboard page

--- public/index.php
+++ public/index.php
@@ -17,14 +17,14 @@ $year = date('Y');
 
 $data = array();
 for ($i = $year - 1; $i >= 2017; $i--) {
-    $data[$i] = "";
+    $data[] = $i;
 }
 
 
 ?>
     <div class="text-center">
-        <a class='btn btn-outline-dark btn-lg' href='yearOverview.php?year=" . explode(" ", $year)[0] . "'>Current Year Summary</a>
-        <a class='btn btn-outline-dark btn-lg ' href='yearForecast.php?year=" . explode(" ", $year)[0] . "'>Current Year Forecast</a>
+        <a class='btn btn-outline-dark btn-lg' href='yearOverview.php?year=<?php echo $year;?>'>Current Year Summary</a>
+        <a class='btn btn-outline-dark btn-lg ' href='yearForecast.php?year=<?php echo $year;?>'>Current Year Forecast</a>
     </div>
     <table class="table table-hover mt-5">
         <thead>
@@ -34,9 +34,9 @@ for ($i = $year - 1; $i >= 2017; $i--) {
         </thead>
         <tbody>
         <?php
-        foreach ($data as $year => $datum) {
+        foreach ($data as $datum) {
             echo "<tr>";
-            echo "<td><a class='btn btn-info' href='yearOverview.php?year=" . explode(" ", $year)[0] . "'>" . $year . "</a></td>";
+            echo "<td><a class='btn btn-info' href='yearOverview.php?year=" . $datum . "'>" . $datum . "</a></td>";
             echo "</tr>";
         }
         ?>

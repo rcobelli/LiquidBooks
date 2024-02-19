ed6bc9925aa1786fb07d6880cb4118ebf5a931a2
Ryan Cobelli <ryan.cobelli@rybel-llc.com>
2024-02-19 11:12:15 -0500

    Fix debug key

--- init.php
+++ init.php
@@ -1,12 +1,11 @@
 <?php
 
 // Support DEBUG cookie
+error_reporting(0);
 if ($_COOKIE['debug'] == 'true') {
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(-1);
-} else {
-    error_reporting(0);
 }
 
 require_once("vendor/autoload.php");
